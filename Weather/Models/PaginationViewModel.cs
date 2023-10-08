a3c774d00ae15b4e0f98c3efd57afc28182c5e95
maloremix <i.aparin@mail.ru>
2023-10-08 20:51:32 +0300

    feat: Комментарии к ViewModel

--- Weather/Models/PaginationViewModel.cs
+++ Weather/Models/PaginationViewModel.cs
@@ -2,23 +2,52 @@
 
 namespace Weather.Models
 {
+    /// <summary>
+    /// Модель представления для пагинации погодных записей.
+    /// </summary>
     public class PaginationViewModel
     {
+        /// <summary>
+        /// Список погодных записей на текущей странице.
+        /// </summary>
         public List<WeatherRecord> Records { get; set; }
+
+        /// <summary>
+        /// Год, по которому производится фильтрация.
+        /// </summary>
         public int Year { get; set; }
+
+        /// <summary>
+        /// Месяц, по которому производится фильтрация.
+        /// </summary>
         public int Month { get; set; }
+
+        /// <summary>
+        /// Номер текущей страницы.
+        /// </summary>
         public int Page { get; set; }
+
+        /// <summary>
+        /// Общее количество страниц.
+        /// </summary>
         public int TotalPages { get; set; }
 
+        /// <summary>
+        /// Показывает, есть ли предыдущая страница.
+        /// </summary>
         public bool HasPreviousPage
         {
             get { return Page > 1; }
         }
 
+        /// <summary>
+        /// Показывает, есть ли следующая страница.
+        /// </summary>
         public bool HasNextPage
         {
             get { return Page < TotalPages; }
         }
     }
+
 
 }
